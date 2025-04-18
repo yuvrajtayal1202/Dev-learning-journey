e3f3d44bdf417ba6c6771b83dee52132ac71afe0
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-04-18 14:27:48 +0530

    Added Sample.json in News.js

--- react/NewMonkey/newsmonkey/src/components/Newsitem.js
+++ react/NewMonkey/newsmonkey/src/components/Newsitem.js
@@ -6,17 +6,17 @@ export class NewItem extends Component {
         console.log("Hello I am a constructor")
     }
   render() {
-    let {title, description, imageUrl, newId} = this.props
+    let {title, description, imageUrl, newsUrl} = this.props
     return (
       <div>
         <div className="card my-3" style={{width: "18rem"}}>
           <img src= {imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title">{title}...</h5>
             <p className="card-text">
-              {description}
+              {description}...
             </p>
-            <a href="/" className="btn btn-sm btn-primary">
+            <a href={newsUrl} className="btn btn-sm btn-primary">
               Read More
             </a>
           </div>
