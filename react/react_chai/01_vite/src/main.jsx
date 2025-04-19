0ad48e4dd52ae5a8a089ac102e9635ec9a41c5fb
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-04-19 14:40:17 +0530

    vedio 4 completed

--- react/react_chai/01_vite/src/main.jsx
+++ react/react_chai/01_vite/src/main.jsx
@@ -1,9 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+// function MyApp(){
+//   return(
+//     <div>
+//       <h1>Custom App</h1>
+//     </div>
+//   )
+// }
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const reactElement = React.createElement(
+ 'a',
+ {href: 'https://google.com', target: '_blank'},
+ 'click me to visit'
+)
+createRoot(document.getElementById('root')).
+render(
+    // <MyApp />
     <App />
-  </StrictMode>,
 )
