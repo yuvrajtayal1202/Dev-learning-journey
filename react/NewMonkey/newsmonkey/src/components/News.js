481037aa891afab953665706b46807e77109340c
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-04-19 07:56:33 +0530

    Added next and previous button so user can navigate through news

--- react/NewMonkey/newsmonkey/src/components/News.js
+++ react/NewMonkey/newsmonkey/src/components/News.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from "react";
 import Newsitem from "./Newsitem";
 
@@ -6,56 +7,87 @@ export default class News extends Component {
     super();
     this.state = {
       articles: [],
-      page: 1
+      page: 1,
     };
   }
 
   async componentDidMount() {
-    let url =
-      "https://newsdata.io/api/1/latest?apikey=pub_81185bc9a9e9cbd86b15c7b33a7e0ddb2dbfd&country=in&language=en";
+    await this.fetchArticles(this.state.page);
+  }
+
+  fetchArticles = async (page) => {
+    const url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=6412af9a3075415e9b2d7a80cdd62cef&page=${page}&pageSize=6`;
+
     try {
       let data = await fetch(url);
       let parsedData = await data.json();
-      console.log("API Response:", parsedData);
+      console.log("parsedData", parsedData);
 
-      // Ensure results is an array
-      const results = Array.isArray(parsedData.results)
-        ? parsedData.results
+      const articles = Array.isArray(parsedData.articles)
+        ? parsedData.articles
         : [];
 
-      this.setState({ articles: results });
+      this.setState({ articles });
     } catch (error) {
       console.error("Failed to fetch articles:", error);
     }
-  }
+  };
+
+  handleNext = async () => {
+    if(Math.ceil(this.state.totalResults/6)){
+
+    }
+    else{
+
+      const nextPage = this.state.page + 1;
+      await this.fetchArticles(nextPage);
+      this.setState({ page: nextPage });
+    }
+  };
+
+  handlePre = async () => {
+    if (this.state.page > 1) {
+      const prevPage = this.state.page - 1;
+      await this.fetchArticles(prevPage);
+      this.setState({ page: prevPage });
+    }
+  };
 
   render() {
     return (
       <div className="container my-3">
         <h2 className="text-center">NewsMonkey - Top Headlines</h2>
         <div className="row">
-          {Array.isArray(this.state.articles) &&
-            this.state.articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.article_id}>
-                  <Newsitem
-                    title={element.title ? element.title.slice(0, 40) : " "}
-                    description={
-                      element.description
-                        ? element.description.slice(0, 100)
-                        : " "
-                    }
-                    imageUrl={element.image_url}
-                    newsUrl={element.link}
-                  />
-                </div>
-              );
-            })}
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <Newsitem
+                  title={element.title ? element.title.slice(0, 40) : " "}
+                  description={
+                    element.description
+                      ? element.description.slice(0, 100)
+                      : " "
+                  }
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
         </div>
 
-        <div className="container d-flex justify-content-between">
-          <button type="button" className="btn btn-dark">Previous</button>
-          <button type="button" className="btn btn-dark">Next</button>
+        <div className="container d-flex justify-content-between my-4">
+          <button
+            type="button"
+            className="btn btn-dark"
+            disabled={this.state.page <= 1}
+            onClick={this.handlePre}
+          >
+            &larr; Previous
+          </button>
+          <button type="button" className="btn btn-dark" onClick={this.handleNext}>
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
