9a3a0f44f613cd7529c9d69947e3a63f9cc75a61
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-04-21 13:20:10 +0530

    Build Bg Changer app more understanded the concept of hooks and tailwind css

--- react/react_chai/04BgChanger/vite.config.js
+++ react/react_chai/04BgChanger/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
 })
