f2b9ce3f994a707239c28af94eba2701d27b3617
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-04-24 16:08:30 +0530

    Project 1 React facts done

---
+++ react/freeCodeCamp/01reactFacts/src/MainContent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function MainContent() {
+    return (
+      <>
+        <h1>Fun facts about React!</h1>
+  
+        <ul>
+          <li>Was first release in 2013</li>
+          <li>Was orginally created by Jordan Walke</li>
+          <li>Has well aver 200K stars on Github</li>
+          <li>Is maintained by Meta</li>
+          <li>Powers thousand of enterprise apps, including mobile apps</li>
+        </ul>
+      </>
+    );
+  }
