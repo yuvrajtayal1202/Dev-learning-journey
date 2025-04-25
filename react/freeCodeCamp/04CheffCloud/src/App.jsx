5951f7076423308eb496a0e566b6e7f1a69e8dcb
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-04-25 20:36:31 +0530

    Header Secotion build

--- react/freeCodeCamp/04CheffCloud/src/App.jsx
+++ react/freeCodeCamp/04CheffCloud/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-     Hii
+    <div className="container">
+
+     <Header/>
+    </div>
     </>
   )
 }
---
+++ react/freeCodeCamp/04CheffCloud/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+    <header className="header">
+
+      <img src="../chef-claude-icon.png" alt="" />
+      <span>Chef Claude</span>
+    </header>
+    </>
+  );
+}
