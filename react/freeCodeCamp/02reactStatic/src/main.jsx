2fbac8b1821c06a4d3404c93d213d214d4167e82
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-04-25 13:15:13 +0530

    Created day 14

--- react/freeCodeCamp/02reactStatic/src/main.jsx
+++ react/freeCodeCamp/02reactStatic/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+import "./index.css"
+const root = createRoot(document.getElementById("root"))
+import  Navabar  from "./com/Navabar";
+import  Main  from "./com/Main";
+root.render(<>
+ <Navabar/>
+ <Main/>
+</>)
