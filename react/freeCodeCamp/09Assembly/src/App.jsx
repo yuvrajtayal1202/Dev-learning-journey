806d38d040c0a9fac60edade5e990019be639942
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-01 14:20:13 +0530

    Assembly Endgame  header section build

--- react/freeCodeCamp/09Assembly/src/App.jsx
+++ react/freeCodeCamp/09Assembly/src/App.jsx
@@ -4,7 +4,14 @@ function App() {
 
   return (
     <>
-    
+     <main className="main">
+      <header className="header">
+        <h1>Assembly: Endgame </h1>
+        <p>Guess the word within 8 attempts to keep the 
+          programming world safe from Assembly!
+        </p>
+      </header>
+     </main>
     </>
   )
 }
