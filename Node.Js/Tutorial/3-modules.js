94e798c8f18358fdcbdc030870544359fc8d67a1
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-13 14:58:56 +0530

    Node OS module learned

--- Node.Js/Tutorial/3-modules.js
+++ Node.Js/Tutorial/3-modules.js
@@ -1,6 +1,9 @@
 // Modules
  const names  = require('./4-names')
  const sayHi  = require('./5-utils')
+const data = require("./6-alternative-flavour")
+require("./7-mind-grind")
+
 
 
 sayHi("yuv")
---
+++ Node.Js/Tutorial/8-os-module.js
@@ -0,0 +1,18 @@
+// import os from "os"
+const os = require('os')
+
+// info about current user
+const user = os.userInfo()
+console.log(user)
+
+// method returns the system uptime in seconds
+console.log(`The System Uptime is ${os.uptime()}`)
+
+const currentOS ={
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem()
+}
+
+console.log(currentOS)
