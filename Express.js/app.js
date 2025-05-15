4f522668812cf16d1c9b14e113f8ac896f25c25c
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-15 18:04:35 +0530

    Express Basic JSON setup

--- Express.js/app.js
+++ Express.js/app.js
@@ -1,20 +1,11 @@
 const express = require('express')
-const path = require('path')
-
 const app = express()
+const { products, people } = require('./data')
 
-// setup static and middleware
-app.use(express.static('./public'))
-
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+app.get('/', (req,res) =>{
+    res.json({products, people})
 })
 
-app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
+app.listen(4000, ()=>{
+    console.log("Server is listening on port 4000")
 })
-
-app.listen(3000, () => {
-  console.log('server is listening on port 3000....')
-})
-
