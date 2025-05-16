f77ae1cafc097d3bfcf56919fd68b5159d6e5829
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-17 02:38:02 +0530

    Express Coruse Completed

--- Express.js/app.js
+++ Express.js/app.js
@@ -1,25 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 // let { people } = require('./data') // Not needed here
 
 // static assets
 // app.use(express.static('./methods-public'))
 // parse form data
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 // parse json
-app.use(express.json())
+app.use(express.json());
 
-const peopleRouter = require('./routes/people') // <-- FIXED
-const authRouter = require('./routes/auth') // <-- FIXED
-app.use('/api/people', peopleRouter) // <-- FIXED
-app.use('/login', authRouter) // <-- FIXED
-
-app.get('/', (req, res) => {
-  res.send('API is working');
-});
-
-
+const peopleRouter = require("./routes/people"); // <-- FIXED
+const authRouter = require("./routes/auth"); // <-- FIXED
+app.use("/api/people", peopleRouter); // <-- FIXED
+app.use("/login", authRouter); // <-- FIXED
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
-})
+  console.log("Server is listening on port 5000....");
+});
