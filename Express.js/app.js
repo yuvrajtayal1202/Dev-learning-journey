9f7839c6431f5b5621d914212adecc8720fdf92d
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-16 12:54:09 +0530

    Creatd day 34

--- Express.js/app.js
+++ Express.js/app.js
@@ -1,11 +1,32 @@
-const express = require('express')
-const app = express()
-const { products, people } = require('./data')
+import express from "express";
+import { products } from "./data.js";
+const app = express();
 
-app.get('/', (req,res) =>{
-    res.json({products, people})
-})
+app.get("/", (req, res) => {
+  res.send('<h1>Home Page</h1><a href = "/api/products">products</a>');
+});
 
-app.listen(4000, ()=>{
-    console.log("Server is listening on port 4000")
-})
+app.get("/api/products", (req, res) => {
+  const newProduct = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProduct);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  const { productID } = req.params;
+
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("Product Does Not Exist");
+  }
+
+  return res.json(singleProduct);
+});
+
+app.listen(800, () => {
+  // console.log("Server is listening on port 6000")
+});
