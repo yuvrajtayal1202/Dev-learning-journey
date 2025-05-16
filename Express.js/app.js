4aac0711cfbf6f9f5267e1278cebad72b48e53fd
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-17 01:28:51 +0530

    Express put method learned

--- Express.js/app.js
+++ Express.js/app.js
@@ -3,12 +3,14 @@ const app = express()
 let { people } = require('./data')
 
 // static assets
-app.use(express.static('./methods-public'))
+// app.use(express.static('./methods-public'))
 // parse form data
 app.use(express.urlencoded({ extended: false }))
 // parse json
 app.use(express.json())
-
+app.get('/', (req, res) => {
+  res.send('API is working');
+});
 
 app.post('/api/people', (req, res) => {
   const { name } = req.body
@@ -36,29 +38,29 @@ app.post('/login', (req, res) => {
     return res.status(200).send(`Welcome ${name}`)
   }
 
-  
+
   res.status(401).send('Please Provide Credentials')
 })
 
-// app.put('/api/people/:id', (req, res) => {
-//   const { id } = req.params
-//   const { name } = req.body
+app.put('/api/people/:id', (req, res) => {
+  const { id } = req.params
+  const { name } = req.body
 
-//   const person = people.find((person) => person.id === Number(id))
+  const person = people.find((person) => person.id === Number(id))
 
-//   if (!person) {
-//     return res
-//       .status(404)
-//       .json({ success: false, msg: `no person with id ${id}` })
-//   }
-//   const newPeople = people.map((person) => {
-//     if (person.id === Number(id)) {
-//       person.name = name
-//     }
-//     return person
-//   })
-//   res.status(200).json({ success: true, data: newPeople })
-// })
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` })
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name
+    }
+    return person
+  })
+  res.status(200).json({ success: true, data: newPeople })
+})
 
 // app.delete('/api/people/:id', (req, res) => {
 //   const person = people.find((person) => person.id === Number(req.params.id))
