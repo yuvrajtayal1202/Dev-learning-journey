99d261ab9480a1228deee3b040a722a9d353f447
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-18 00:42:12 +0530

    JWT(Auth) first Login Setup build

--- 7-JWT(Auth)/server.js
+++ 7-JWT(Auth)/server.js
@@ -1,19 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+require("dotenv").config(); // <-- Add this line
 
+app.use(express.json()); // <-- Add this line
 
 const posts = [
-    {
-        username: "yuvraj",
-        title: "Post 1"
-    },  {
-        username: "jim",
-        title: "Post 2"
-    }
-]
+  {
+    username: "yuvraj",
+    title: "Post 1",
+  },
+  {
+    username: "jim",
+    title: "Post 2",
+  },
+];
 
-app.get('/posts', (req, res) => {
-    res.send(posts)
-})
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
-app.listen(5000)
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const user = { name: username };
+  
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
+});
+
+app.listen(5000);
