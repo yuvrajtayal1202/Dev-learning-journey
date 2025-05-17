0f8036ac698441bca544bd9f6c480ffc68d85805
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-17 08:14:07 +0530

    MongoDB Insert one learned

---
+++ 6-MongoDB/01setup.js
@@ -0,0 +1,7 @@
+show db   // SHow all the data bases
+use employees // use employee data base
+
+// C - create
+// R - read
+// U - update
+// D - delete
---
+++ 6-MongoDB/02create.js
@@ -0,0 +1,10 @@
+db.inventory.insertOne({ item: "canvas", qty: 100, tags: ["cotton"] });
+
+db.inventory.insertMany([
+  { item: "canvas1", qty: 100, tags: ["cotton"] },
+  { item: "canvas2", qty: 100, tags: ["cotton"] },
+  { item: "canvas3", qty: 100, tags: ["cotton"] },
+  { item: "canvas4", qty: 100, tags: ["cotton"] },
+  { item: "canvas5", qty: 100, tags: ["cotton"] },
+  { item: "canvas6", qty: 100, tags: ["cotton"] },
+]);
