9cdccf18f0f0779704fc871ce6e9a520318329c6
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-19 13:22:27 +0530

    Next.js Layout and Children Learned

--- 8-Next.js/01basics/app/layout.tsx
+++ 8-Next.js/01basics/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h1 className="text-5xl">Root</h1>
         {children}
       </body>
     </html>
