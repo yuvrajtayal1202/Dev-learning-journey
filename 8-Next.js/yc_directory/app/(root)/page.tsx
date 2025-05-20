9d93b733e46a37c39125b18d8b91e5e8e1a3d7d7
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-20 18:05:40 +0530

    Next.js All Startup

--- 8-Next.js/yc_directory/app/(root)/page.tsx
+++ 8-Next.js/yc_directory/app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
@@ -6,6 +7,22 @@ export default async function Home({
   searchParams: Promise<{ query: string }>;
 }) {
   const query = (await searchParams).query;
+
+
+const posts = [
+{
+_createdAt: new Date(),
+views: 55,
+author: { _id: 1 },
+_id: 1,
+description: "This is a description",
+image:
+"https://images.unsplash.com/photo-1634912314704-c646c586b131?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.03&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+category: "Robots",
+title: "We Robots",
+},
+];
+
   return (
     <>
       <section className="pink_container ">
@@ -20,6 +37,22 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search Result for "${query}"` : 'All Startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
