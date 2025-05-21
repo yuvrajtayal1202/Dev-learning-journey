2dd79e3744b83281160a50ff3ea6eeac4fd73302
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-21 06:46:40 +0530

    Next,js Fetching Data and types

--- 8-Next.js/yc_directory/app/(root)/page.tsx
+++ 8-Next.js/yc_directory/app/(root)/page.tsx
@@ -1,5 +1,7 @@
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/query";
 
 export default async function Home({
   searchParams,
@@ -8,20 +10,21 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
 
-
-const posts = [
-{
-_createdAt: new Date(),
-views: 55,
-author: { _id: 1 },
-_id: 1,
-description: "This is a description",
-image:
-"https://images.unsplash.com/photo-1634912314704-c646c586b131?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.03&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-category: "Robots",
-title: "We Robots",
-},
-];
+   const posts = await client.fetch(STARTUPS_QUERY);
+   
+// const posts = [
+// {
+// _createdAt: new Date(),
+// views: 55,
+// author: { _id: 1 },
+// _id: 1,
+// description: "This is a description",
+// image:
+// "https://images.unsplash.com/photo-1634912314704-c646c586b131?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.03&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+// category: "Robots",
+// title: "We Robots",
+// },
+// ];
 
   return (
     <>
--- 8-Next.js/yc_directory/components/StartupCard.tsx
+++ 8-Next.js/yc_directory/components/StartupCard.tsx
@@ -3,10 +3,10 @@ import { EyeIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-// import { Author, Startup } from "@/sanity/types";
-// import { Skeleton } from "@/components/ui/skeleton";
+import { Author, Startup } from "@/sanity/types";
+// import { Skeleton } from "@/components/ui/skeleton";  
 
-// export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
+export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
   const {
