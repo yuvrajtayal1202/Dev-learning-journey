3f17d7cfaf9bac307d321db7e1faff1e3d902dc9
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-21 11:10:13 +0530

    Next.js dyncamic routes

---
+++ 8-Next.js/routing-demo/src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Blog Page!
+    </div>
+  )
+}
+
+export default page
---
+++ 8-Next.js/routing-demo/src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1>
+
+      Hello World!!
+        </h1>
+    </div>
+  )
+}
+
+export default page
