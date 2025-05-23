b5ce3cb2c6cdb5a107c9acff2272ebb6fb7565b1
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-23 16:42:53 +0530

    Next.js Post

--- 8-Next.js/route-handlers/src/app/comments/route.ts
+++ 8-Next.js/route-handlers/src/app/comments/route.ts
@@ -3,3 +3,13 @@ import {comments }from './data'
 export async function GET() {
     return Response.json(comments);
 }
+
+
+export async function POST(req: Request) {
+ const comment =  await req.json()
+ const newComment = {
+    id: comments.length + 1,
+     text: comment.text
+ }
+ comments.push(newComment)
+}
