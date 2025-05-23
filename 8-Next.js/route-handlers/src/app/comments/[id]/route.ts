143713ef9ac327665d583894ea82cd3e1d61a39d
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-23 17:07:29 +0530

    Next.js DELETE

--- 8-Next.js/route-handlers/src/app/comments/[id]/route.ts
+++ 8-Next.js/route-handlers/src/app/comments/[id]/route.ts
@@ -1,18 +1,28 @@
-import {comments }from '../data'
+import { comments } from '../data'
 
-export async function GET(_req : Request, {params}: {params: Promise<{id: string}>}) {
+export async function GET(_req: Request, { params }: { params: Promise<{ id: string }> }) {
     const { id } = await params
     const comment = comments.find((comment) => comment.id === parseInt(id))
     return Response.json(comment)
 }
 
 
-export async function PATCH(req : Request, {params}: {params: Promise<{id: string}>}) {
+export async function PATCH(req: Request, { params }: { params: Promise<{ id: string }> }) {
     const { id } = await params
     const body = await req.json();
-    const {text} = body
+    const { text } = body
 
     const index = comments.findIndex((comment) => comment.id === parseInt(id))
     comments[index].text = text
     return Response.json(comments[index]);
+}
+
+
+export async function DELETE(req: Request, { params }: { params: Promise<{ id: string }> }) {
+   
+       const { id } = await params
+           const index = comments.findIndex((comment) => comment.id === parseInt(id))
+        const deletedComment = comments[index]
+        comments.splice(index, 1)
+        return Response.json(deletedComment);
 }
