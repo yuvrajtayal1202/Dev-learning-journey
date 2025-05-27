e0f467bee8792d1c29df0456bbc4015d8f1421c7
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-28 02:13:28 +0530

    Shadcn setup

---
+++ 10-ShadcnUI/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { LoginForm } from './components/login-form'
+import { Button } from './components/ui/button'
+function App() {
+
+  return (
+    <>
+      <h1>Shadcn Components</h1>
+      
+    <Button>Prima</Button>
+    <LoginForm/>
+     </>
+  )
+}
+
+export default App
